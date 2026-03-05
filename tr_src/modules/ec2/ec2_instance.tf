673abf2bbb5c1a66271aede5122a3bc2493a4d8d
tatekitzhak <tatekosi@gmail.com>
2026-03-05 12:14:34 -0800

    Deploy full with ssm

--- tr_src/modules/ec2/ec2_instance.tf
+++ tr_src/modules/ec2/ec2_instance.tf
@@ -17,8 +17,8 @@ resource "aws_instance" "ubuntu_ec2_instance_terraform" {
   subnet_id                   = aws_subnet.tf_subnet_public.id
   vpc_security_group_ids      = [var.sg_id]
   associate_public_ip_address = true
-  # iam_instance_profile        = aws_iam_instance_profile.ec2_ssm.name
-  # key_name                    = aws_key_pair.deployer.key_name
+  iam_instance_profile        = aws_iam_instance_profile.ec2_ssm.name
+  key_name                    = aws_key_pair.deployer.key_name
 
   tags = {
     Name = "aws-ec2-instance-terraform-${local.formatted_time}"
